faae0c2929475a7eefb4e39a3defe48476bb3127
vanquiche <stevevang2010@hotmail.com>
2021-08-11 16:56:03 -0700

    renamed routes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -8,7 +8,8 @@ router.get('/', adminController.index);
 router.get('/new-blog', adminController.get_new_blog);
 router.post('/new-blog', adminController.post_new_blog);
 
-router.get('/show-posts', adminController.show_blogs);
+router.get('/blog', adminController.show_blogs);
+router.get('/blog/:id', adminController.get_blog)
 
 router.get('/profile', adminController.profile);
 
