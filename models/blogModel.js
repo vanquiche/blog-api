c70b0eb14f90f4baee6d9d7bdd763932cdbd22be
vanquiche <stevevang2010@hotmail.com>
2021-08-11 16:55:48 -0700

    add url virtual

--- models/blogModel.js
+++ models/blogModel.js
@@ -6,7 +6,11 @@ const blog = new Schema({
   body: { type: String, required: true },
   comments: { type: Array },
   createdAt: { type: Date, default: Date.now, required: true },
-  publishedAt: { type: Date },
+  published: { type: Boolean, default: false },
+});
+
+blog.virtual('url').get(function () {
+  return '/blog/' + this._id;
 });
 
 module.exports = mongoose.model('Blog', blog);
