39934e8e30ebf0dcb85896b1661fd96a82e13a12
vanquiche <stevevang2010@hotmail.com>
2021-08-13 17:38:57 -0700

    install JWT and add authentiation and set cookie

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 if (process.env.NODE_MODE !== 'production') {
   dotenv.config();
 }
+const jwt = require('jsonwebtoken');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,7 +10,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const blogRouter = require('./routes/blogRoute');
 const adminRouter = require('./routes/adminRoute');
 const loginRouter = require('./routes/loginRoute');
@@ -17,13 +17,35 @@ const loginRouter = require('./routes/loginRoute');
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = process.env.MONGO_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// middleware
+// verify client jsonwebtoken
+const authenticateToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect('/login');
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      res.status(403).redirect('/login');
+    }
+    req.user = user;
+    return next();
+  });
+};
+// assign locals.user if current user exist
+const showUser = (req, res, next) => {
+  if (res.locals.user) {
+    res.redirect('/admin');
+  } else {
+    return next();
+  }
+};
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -36,9 +58,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/blog', blogRouter);
-app.use('/admin', adminRouter);
+app.use('/admin', authenticateToken, adminRouter);
 app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
