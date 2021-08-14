15016d93837a32f819483b02fa06a092e538b021
vanquiche <stevevang2010@hotmail.com>
2021-08-13 17:42:32 -0700

    add post route

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const loginController = require('../controller/loginController')
 
 router.get('/', loginController.login_get);
+router.post('/', loginController.login_submit_post)
 
 module.exports = router;
