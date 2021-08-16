a7e6f7e4fc39510282818dad9aca11eefed10d10
vanquiche <stevevang2010@hotmail.com>
2021-08-15 19:40:49 -0700

    add role to admin schema

--- models/adminModel.js
+++ models/adminModel.js
@@ -7,6 +7,7 @@ const admin = new Schema({
   lastname: {type: String, required: true},
   username: {type: String, required: true},
   password: {type: String, required: true},
+  role: {type: String, default: 'admin', required: true},
   createdAt: {type: Date, require: true}
 })
 
