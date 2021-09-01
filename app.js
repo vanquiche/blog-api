0ae37a012fcacf387fa66595a41b79cf50646833
vanquiche <stevevang2010@hotmail.com>
2021-08-31 22:50:01 -0700

    add compression and helmet middleware for security and performance

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const jwt = require('jsonwebtoken');
 const methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 const apiRouter = require('./routes/apiRoute');
@@ -55,10 +57,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
